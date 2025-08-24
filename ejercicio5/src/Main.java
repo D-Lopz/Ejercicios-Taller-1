0e887a6b3062e16afd8897fc60ed14b5ee013890
David Lopez <davidlopezing.1@gmail.com>
2025-08-24 12:52:32 -0500

    Ejercicios taller E5 arreglado

--- ejercicio5/src/Main.java
+++ ejercicio5/src/Main.java
@@ -5,19 +5,19 @@ public class Main {
 
         Scanner dL = new Scanner(System.in);
         int[] n = new int[10];
-        double maximo = n[0];
-        double minimo = n[0];
+        int maximo = n[0];
 
         System.out.println("Ingrese los 10 números enteros");
 
         for (int i = 0; i < n.length; i++) {
 
-            System.out.print("Número: " + (i + 1) + ": ");
+            System.out.print("Número " + (i + 1) + ": ");
             n[i] = dL.nextInt();
 
         }
 
-        for (double element : n) {
+        int minimo = n[0];
+        for (int element : n) {
             if (element < minimo) {
                 minimo = element;
             }
